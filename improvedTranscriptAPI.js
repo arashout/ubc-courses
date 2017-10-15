fb104d4b4e4a90c17827c7762a56bda0e3cf83d8
Arash Outadi <arash.out@gmail.com>
2017-10-15 21:39:43 +0100

    Modernizing

--- improvedTranscriptAPI.js
+++ improvedTranscriptAPI.js
@@ -1,17 +1,17 @@
-var COL_INDEX = Object.freeze({
+const COL_INDEX = Object.freeze({
     COURSE_CODE: 0,
     LETTER_GRADE: 3,
     STANDING: 10
 });
 
 function replaceNbsps(str) {
-  var re = new RegExp(String.fromCharCode(160), 'g');
+  const re = new RegExp(String.fromCharCode(160), 'g');
   return str.replace(re, ' ');
 }
 
 function httpGetAsync(url, callback)
 {
-    var xmlHttp = new XMLHttpRequest();
+    let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
@@ -21,12 +21,12 @@ function httpGetAsync(url, callback)
 }
 
 /**
- * Ridiculous name! Function that returns and callback function for changing the entry element to the course name
+ * Ridiculous name! Function that returns a callback function for changing the entry element to the course name
  * @param {*} element 
  */
 function createChangeCourseNameCallback(element){
     return function(response){
-        var course = JSON.parse(response);
+        let course = JSON.parse(response);
         if(course['name'] !== undefined){
             element['innerText'] = course['name'];
         }
@@ -37,8 +37,7 @@ function createChangeCourseNameCallback(element){
     }
 }
 
-function formatGradeSummary(){
-    var iframe = document.querySelector('#iframe-main').contentWindow.document;
+function formatGradeSummary(iframeElement){
     // CREDIT: Original formatting code is from
     // https://github.com/crclayton
     // https://github.com/crclayton/ubc-unofficial-transcript-exporter
@@ -54,44 +53,44 @@ function formatGradeSummary(){
     [ '#calculator_title', 
     '#calculator_title_text', 
     '.ui-tabs-nav'].forEach(function(n){
-        iframe.querySelector(n).remove();
+        iframeElement.querySelector(n).remove();
     });
 
     // space things out a bit better 
-    iframe.querySelector('h1').style = 'margin:10px 0 -20px 70px;'
-    iframe.querySelector('#header-invisible img').style = 'margin-bottom: 15px;'
+    iframeElement.querySelector('h1').style = 'margin:10px 0 -20px 70px;'
+    iframeElement.querySelector('#header-invisible img').style = 'margin-bottom: 15px;'
 
     // center table and set to width of UBC header image 
-    iframe.querySelector('#tabs').style = 'margin: 0px auto; width:800px';
-    return iframe;
+    iframeElement.querySelector('#tabs').style = 'margin: 0px auto; width:800px';
 }
 
-var iframe = formatGradeSummary();
+let iframe = document.querySelector('#iframe-main').contentWindow.document;
+formatGradeSummary();
 
-var tableElement = iframe.getElementById('allSessionsGrades');
-var tableBody = tableElement.children[0];
-var tableRows = tableBody.children;
-var n = tableRows.length;
+const tableElement = iframe.getElementById('allSessionsGrades');
+const tableBody = tableElement.children[0];
+const tableRows = tableBody.children;
+const n = tableRows.length;
 
 var rootAPIUrl = 'https://arashout.pythonanywhere.com/course/api/v1.0/code/';
 
-for(var i = tableRows.length - 1; i >= 0; i--){
-    var row = tableRows[i];
+for(let i = tableRows.length - 1; i >= 0; i--){
+    let row = tableRows[i];
     row.removeChild(row.children[COL_INDEX.STANDING]);
-    var courseCode = replaceNbsps(row.children[COL_INDEX.COURSE_CODE].innerText);
-    var letterGrade = row.children[COL_INDEX.LETTER_GRADE].innerText;
+    let courseCode = replaceNbsps(row.children[COL_INDEX.COURSE_CODE].innerText);
+    let letterGrade = row.children[COL_INDEX.LETTER_GRADE].innerText;
     if(letterGrade === ''){
         tableBody.removeChild(row);
     }
     else{
-        var entry = row.insertCell(1);
+        let entry = row.insertCell(1);
         entry.style.textAlign = 'center';
         if(i === 0){
             entry.innerText = 'Course Name';
             entry.classList.add('listHeader');
         }
         else{
-            var apiURL = rootAPIUrl + courseCode;
+            let apiURL = rootAPIUrl + courseCode;
             httpGetAsync(apiURL, createChangeCourseNameCallback(entry));
             entry.classList.add('listRow');
         }
