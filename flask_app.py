6db382f43b40b8a4096312477f4d29e94d93f60a
Arash Outadi <arash.outadi@ravelin.com>
2017-12-30 19:36:32 +0000

    Adding digest compatiblitity

--- flask_app.py
+++ flask_app.py
@@ -2,12 +2,17 @@ from flask import Flask, jsonify, render_template, request
 from flask_sslify import SSLify
 import json
 import os
+import re
 
 app = Flask(__name__)
 sslify = SSLify(app)
 
-VERSION = '1.2';
-VERSION_KEY = 'version_key';
+pattern_course = re.compile(r'c\d+')
+
+VERSION = '1.2'
+VERSION_KEY = 'version_key'
+
+DIGEST_KEY = 'digest_key'
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 PATH_JSON_COURSES = os.path.join(CURRENT_DIRECTORY, 'data', 'courses.json')
@@ -30,17 +35,20 @@ def get_task(course_code):
 
 @app.route('/courses', methods=['GET'])
 def get_courses():
-    all_args = request.args.to_dict()
+    all_args: dict = request.args.to_dict()
 
     response_dict = {}
 
     if VERSION_KEY in all_args:
         response_dict[VERSION_KEY] = VERSION
-        # Delete the query key so we don't iterate over it below
-        del all_args[VERSION_KEY]
+    
+    # TODO: Use this to prevent many requests
+    if DIGEST_KEY in all_args:
+        pass
 
-    for _, course_code in all_args.items():
-        if course_code in course_dictionary:
+    for key, value in all_args.items():
+        if (isCourseQueryParam(key) and value in course_dictionary):
+            course_code = value
             response_dict[course_code] = course_dictionary[course_code]
 
     return jsonify(response_dict)
@@ -56,6 +64,11 @@ def apply_caching(response):
     response.headers['Access-Control-Allow-Methods'] = 'GET'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
+
+def isCourseQueryParam(param_key: str) -> bool:
+    if pattern_course.match(param_key) is not None:
+        return True
+    return False
 
 if __name__ == '__main__':
     app.run()
