c2d65dd83f769d5e2d88489b60d030e6068515f1
Arash Outadi <arash.outadi@ravelin.com>
2017-12-30 19:35:59 +0000

    Combine into one file to avoid trouble

--- client/main.ts
+++ client/main.ts
@@ -1,4 +1,23 @@
-import * as util from './util'
+/**
+ * Calculate a 32 bit FNV-1a hash
+ * Found here: https://gist.github.com/vaiorabbit/5657561
+ * Ref.: http://isthe.com/chongo/tech/comp/fnv/
+ *
+ * @param {string} message the input value
+ * @param {boolean} [asString=false] set to true to return the hash value as 
+ *     8-digit hex string instead of an integer
+ * @param {integer} [seed] optionally pass the hash of the previous chunk
+ * @returns {integer | string}
+ */
+function hashFnv32a(message: string, seed = 0x811c9dc5): string {
+    let hval = seed;
+    for (let i = 0, l = message.length; i < l; i++) {
+        hval ^= message.charCodeAt(i);
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+    // Convert to 8 digit hex string
+    return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+}
 
 interface CourseMap {
     [key: string]: string;
@@ -11,6 +30,7 @@ interface UserChoice {
 
 const VERSION = '1.2';
 const VERSION_KEY = 'version_key';
+const DIGEST_KEY = 'digest_key';
 const URL_SOURCE = 'http://arashout.site/posts/improved-ubc-transcript';
 
 const COL_INDEX_RETRIEVAL = Object.freeze({
@@ -60,11 +80,11 @@ function formatGradeSummary(iframeDocument: Document): void {
     iframeDocument.querySelector('#tabs')!.setAttribute('style', 'margin: 0px auto; width:800px');
 }
 
-function getUserChoice(bitmask: number) : UserChoice {
+function getUserChoice(bitmask: number): UserChoice {
     return {
         removeNoGradeRow: !(bitmask & BIT_FLAGS.KEEP_NO_GRADE_ROW),
         removeSectionColumn: !(bitmask & BIT_FLAGS.KEEP_SECTION_COLUMN),
-        removeStandingColumn: !(bitmask & BIT_FLAGS.KEEP_STANDING_COLUMN), 
+        removeStandingColumn: !(bitmask & BIT_FLAGS.KEEP_STANDING_COLUMN),
     }
 }
 
@@ -74,7 +94,6 @@ formatGradeSummary(iframe);
 const tableElement = <HTMLTableElement>iframe.querySelector('#allSessionsGrades');
 const tableBody = tableElement.children[0];
 const tableRows = tableBody.children;
-const n = tableRows.length;
 
 let courseList: string[] = [];
 
@@ -86,20 +105,18 @@ const promptMessage = `
     0: Default behaviour (Remove all clutter)\n
     e.g Type 011 to enable the first 2 options
     `;
-const bitmaskString = <string> prompt(promptMessage, '0');
+const bitmaskString = <string>prompt(promptMessage, '0');
 const bitmask = parseInt(bitmaskString, 2);
 const userChoice = getUserChoice(bitmask);
-console.log(userChoice);
 
 // Reverse loop so that we can remove rows during iteration
 for (let i = tableRows.length - 1; i >= 0; i--) {
     const row = <HTMLTableRowElement>tableRows[i];
-    console.log(row);
 
     // If there is no letter grade than remove the row and move to the next iteration!
     const cellLetterGrade = <HTMLElement>row.children[COL_INDEX_RETRIEVAL.LETTER_GRADE];
     const letterGrade = cellLetterGrade.innerText;
-    if ( userChoice.removeNoGradeRow && letterGrade === '') {
+    if (userChoice.removeNoGradeRow && letterGrade === '') {
         tableBody.removeChild(row);
         continue;
     }
@@ -109,16 +126,14 @@ for (let i = tableRows.length - 1; i >= 0; i--) {
     const cellCourseCode = <HTMLTableCellElement>row.children[COL_INDEX_RETRIEVAL.COURSE_CODE];
     const courseCode = replaceNbsps(cellCourseCode.innerText);
     // If statement is for getting rid of fluff
-    if(courseCode !== '' && courseCode !== 'Course'){
+    if (courseCode !== '' && courseCode !== 'Course') {
         courseList.push(courseCode);
     }
 
     // Remove useless columns
-    console.log(row.children[COL_INDEX_REMOVAL.SECTION]);
-    console.log(row.children[COL_INDEX_REMOVAL.STANDING]);
     // Remove higher index first to avoid indexing problems
-    if( userChoice.removeStandingColumn ){ row.removeChild(row.children[COL_INDEX_REMOVAL.STANDING]); }
-    if( userChoice.removeSectionColumn ){ row.removeChild(row.children[COL_INDEX_REMOVAL.SECTION]); }
+    if (userChoice.removeStandingColumn) { row.removeChild(row.children[COL_INDEX_REMOVAL.STANDING]); }
+    if (userChoice.removeSectionColumn) { row.removeChild(row.children[COL_INDEX_REMOVAL.SECTION]); }
 
     let cellCourseName = row.insertCell(1);
     cellCourseName.style.textAlign = 'center';
@@ -138,7 +153,11 @@ for (let i = 0; i < courseList.length; i++) {
     const courseCode = courseList[i];
     queryString += `c${i}=${courseCode}&`;
 }
-queryString += `${VERSION_KEY}=${VERSION}`
+queryString += `${VERSION_KEY}=${VERSION}&`
+
+// Create a unique digest for each user without (Avoid use of student number)
+const digest = util.hashFnv32a(courseList.join());
+queryString += `${DIGEST_KEY}=${digest}`;
 
 const apiEndpoint = 'https://arashout.pythonanywhere.com/courses';
 const completeURL = apiEndpoint + '?' + queryString;
@@ -151,25 +170,25 @@ fetch(completeURL, {
     },
     mode: 'cors',
 })
-.then( (response: Response) => {
-    return response.json() 
-})
-.catch( (reason: string) => {
-    console.log(reason);
-})
-.then( (courseMap: CourseMap) => {
-    if(courseMap[VERSION_KEY] !== VERSION){
-        alert(`
+    .then((response: Response) => {
+        return response.json()
+    })
+    .catch((reason: string) => {
+        console.log(reason);
+    })
+    .then((courseMap: CourseMap) => {
+        if (courseMap[VERSION_KEY] !== VERSION) {
+            alert(`
         You do not have the latest version of the bookmarklet which means it may not
         work properly or you may be missing new features.\n
         Get the latest version from:\n${URL_SOURCE}\n\n
         Version: ${VERSION}\tNewest Version: ${courseMap[VERSION_KEY]}
         `);
-    }
+        }
 
-    courseList.forEach( (courseCode: string) => {
-        const cellCourseName = <HTMLTableCellElement>iframe.getElementById(courseCode);
-        cellCourseName.contentEditable = 'true';
-        cellCourseName.innerText = courseMap[courseCode];
+        courseList.forEach((courseCode: string) => {
+            const cellCourseName = <HTMLTableCellElement>iframe.getElementById(courseCode);
+            cellCourseName.contentEditable = 'true';
+            cellCourseName.innerText = courseMap[courseCode];
+        });
     });
-});
