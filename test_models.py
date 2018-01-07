83f73f5fed090b0d8ef27f28daa6a6b3635e7be9
Arash Outadi <arash.out@gmail.com>
2018-01-07 14:43:49 +0000

    Added tests for daowrapper

--- test_models.py
+++ test_models.py
@@ -9,40 +9,13 @@ class CourseTest(models.AbstractCourse):
         'collection': 'courses-test'
     }
 
+
 COURSE_DICT = {
-    'APSC150': ' Engineering Case Studies',
-    'APSC160': 'Introduction to Computation in Engineering Design',
-    'CHEM154': 'Chemistry for Engineering',
-    'CIVL200': 'Engineering and Sustainable Development',
-    'ENGL112': 'Strategies for University Writing',
-    'EOSC114': 'The Catastrophic Earth: Natural Disasters',
-    'LING101': 'Languages of the World',
     'MATH100': 'Differential Calculus with Applications to Physical Sciences and Engineering',
     'MATH101': 'Integral Calculus with Applications to Physical Sciences and Engineering',
-    'MATH152': 'Linear Systems',
-    'MATH253': 'Multivariable Calculus',
-    'MATH255': 'Ordinary Differential Equations',
-    'MATH307': 'Applied Linear Algebra',
-    'MECH220': 'Technical Skills Practicum',
-    'MECH221': 'Engineering Science I',
-    'MECH222': 'Engineering Science II',
-    'MECH223': 'Introduction to the Mechanical Design Process',
-    'MECH224': 'Integration of Engineering Concepts I',
-    'MECH225': 'Integration of Engineering Concepts II',
-    'MECH226': 'Technical Communication for Mechanical Engineers',
-    'MECH305': 'Data Analysis and Mechanical Engineering Laboratories',
-    'MECH325': 'Mechanical Design I',
-    'MECH326': 'Mechanical Design II',
-    'MECH328': 'Mechanical Engineering Design Project',
-    'MECH358': 'Engineering Analysis',
-    'MECH360': 'Mechanics of Materials',
-    'MECH368': 'Engineering Measurements and Instrumentation',
-    'MECH375': 'Heat Transfer',
-    'MECH380': 'Fluid Dynamics',
-    'PHIL120': 'Introduction to Critical Thinking',
-    'PHYS153': ' Elements of Physics',
-    'PHYS170': 'Mechanics I'
+    'MATH152': 'Linear Systems'
 }
+
 
 class TestDAO(unittest.TestCase):
     def setUp(self):
@@ -55,12 +28,25 @@ class TestDAO(unittest.TestCase):
         )
         # Assuming insert works properly
         models.insert_courses_from_dict(self.dao_wrapper, COURSE_DICT)
-    
+
     def tearDown(self):
         self.dao_wrapper.drop_collection(CourseTest)
 
+    def assert_is_sorted(self, container: list, ascending=False):
+        if not container or len(container) == 1:
+            self.fail('Container is empty or only has one lelement')
+
+        prev = container[0]
+        for i in range(1, len(container)):
+            cur = container[i]
+            if(ascending):
+                self.assertLessEqual(prev, cur)
+            else:
+                self.assertLessEqual(cur, prev)
+            prev = cur
+
     def test_get(self):
-        code = 'MATH101'
+        code = 'MATH152'
         c: models.AbstractCourse = self.dao_wrapper.get(code)
         self.assertIsNotNone(c)
         self.assertEqual(c.name, COURSE_DICT.get(code))
@@ -75,14 +61,14 @@ class TestDAO(unittest.TestCase):
         c: models.AbstractCourse = self.dao_wrapper.update_course(code, name)
         self.assertIsNotNone(c)
         self.assertEqual(c.name, name)
-    
+
     def test_update_exact_match(self):
         code = 'MATH152'
-        
+
         c: models.AbstractCourse = self.dao_wrapper.update_course(code, COURSE_DICT[code])
         self.assertIsNotNone(c)
         self.assertEqual(c.name, COURSE_DICT[code])
-    
+
     def test_update_new_name(self):
         code = 'MATH152'
         name = 'Test1'
@@ -91,31 +77,64 @@ class TestDAO(unittest.TestCase):
         self.assertIsNotNone(c)
         course_name_scores: typing.List[models.CourseNameScore] = c.course_name_scores
         # New suggested name
-        self.assertEqual( len(course_name_scores), 1)
+        self.assertEqual(len(course_name_scores), 1)
         self.assertEqual(course_name_scores[0].name, name)
         self.assertEqual(course_name_scores[0].score, 1)
-    
+
     def test_update_new_name_scoring(self):
         code = 'MATH152'
         name = 'Test1'
 
-        c: models.AbstractCourse = self.dao_wrapper.update_course(code, name)
-        course_name_scores: typing.List[models.CourseNameScore] = c.course_name_scores
-
-        for i in range(1, models.MAX_NAME_COUNT):
+        for i in range(0, models.SCORE_THRESHOLD):
             c: models.AbstractCourse = self.dao_wrapper.update_course(code, name)
             course_name_scores: typing.List[models.CourseNameScore] = c.course_name_scores
-            self.assertEqual( len(course_name_scores), 1) # Length should not change
+            # Length should not change
+            self.assertEqual(len(course_name_scores), 1)
             self.assertEqual(course_name_scores[0].name, name)
-            self.assertEqual(course_name_scores[0].score, i+1)
-        
-        # Next update will make this name the default and pop it off the course name list
+            self.assertEqual(course_name_scores[0].score, i + 1)
+
+        # Next update will make this name the default and pop it off the course
+        # name list
         c: models.AbstractCourse = self.dao_wrapper.update_course(code, name)
         self.assertEqual(c.name, name)
         course_name_scores: typing.List[models.CourseNameScore] = c.course_name_scores
-        self.assertEqual( len(course_name_scores), 0) # Length should not change
-    
+        self.assertEqual(len(course_name_scores), 0)
 
+    def test_too_many_names(self):
+        code = 'MATH152'
+
+        for i in range(0, models.MAX_NAME_COUNT):
+            name = 'TestTitle' + str(i)
+            c: models.AbstractCourse = self.dao_wrapper.update_course(code, name)
+            course_name_scores: typing.List[models.CourseNameScore] = c.course_name_scores
+            self.assertEqual(len(course_name_scores), i + 1)
+
+        # Next name addition should pop the last name addition off since they
+        # have the same popularity
+        popped_name = name
+        name = 'TestTitle' + str(i + 1)
+        c: models.AbstractCourse = self.dao_wrapper.update_course(code, name)
+        course_name_scores: typing.List[models.CourseNameScore] = c.course_name_scores
+        self.assertEqual(len(course_name_scores),  models.MAX_NAME_COUNT)
+        course_names = list(map(lambda cns: cns.name, course_name_scores))
+        self.assertIn(name, course_names)
+        self.assertNotIn(popped_name, course_names)
+
+    def test_popularity_order(self):
+        code = 'MATH152'
+
+        def add_name_n_times(_name, n: int):
+            for i in range(0,n):
+                self.dao_wrapper.update_course(code, _name)
+
+        for i in range(0, models.MAX_NAME_COUNT-1):
+            add_name_n_times('Test' + str(i), i)
+
+        c: models.AbstractCourse = self.dao_wrapper.get(code)
+        print(c.course_name_scores)
+        self.assert_is_sorted(
+            list(map(lambda cns: cns.score, c.course_name_scores))
+        )
 
 
 if __name__ == '__main__':
