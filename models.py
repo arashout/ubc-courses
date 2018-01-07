966bd25719f7697cd1d510e6ee2f7d55da18ebc1
Arash Outadi <arash.out@gmail.com>
2018-01-07 14:56:11 +0000

    Add tests for dao logic (#5)
    * Add comments and create abstract course class
    
    * Make models more generic and add testing file
    
    * Fixed logic error and changed thresholds
    
    * Added tests for daowrapper
    
    * pipenv updates

--- models.py
+++ models.py
@@ -9,8 +9,8 @@ PATH_JSON_COURSES = os.path.join(CURRENT_DIRECTORY, 'data', 'courses.json')
 
 DB_NAME = 'ubcapi'
 
-SCORE_THRESHOLD = 3
-MAX_NAME_COUNT = 3
+SCORE_THRESHOLD = 4
+MAX_NAME_COUNT = 10
 
 
 class CourseNameScore(me.EmbeddedDocument):
@@ -31,7 +31,7 @@ class CourseNameScore(me.EmbeddedDocument):
         return "{0} : {1}".format(self.name, self.score)
 
 
-class Course(me.Document):
+class AbstractCourse(me.Document):
     code = me.StringField(primary_key=True)
     name = me.StringField(required=True)
     course_name_scores = me.ListField(
@@ -47,9 +47,20 @@ class Course(me.Document):
     def __repr__(self):
         return "{0} : {1} -> {2}".format(self.code, self.name, self.course_name_scores)
 
+    meta = {
+        'allow_inheritance': True,
+        'abstract': True
+    }
+
+
+class Course(AbstractCourse):
+    meta = {
+        'collection': 'courses-live'
+    }
+
 
 class DAOWrapper:
-    def __init__(self, db_user, db_password, db_host, db_port):
+    def __init__(self, db_user, db_password, db_host, db_port, generic_course: AbstractCourse):
         uri = "mongodb://{0}:{1}@{2}:{3}/{4}".format(
             db_user,
             db_password,
@@ -58,70 +69,78 @@ class DAOWrapper:
             DB_NAME
         )
         # NOTE: 'connect=False' is to avoid connection pooling sine PyMongo is not fork-safe
-        # Basically it will make PythonAnywhere setup work...
         me.connect(host=uri, connect=False, maxPoolSize=1)
+        self.generic_course = generic_course
 
-    def insert_many(self, courses: typing.List[Course]):
-        Course.objects.insert(courses)
+    def insert_many(self, courses: typing.List[AbstractCourse]):
+        try:
+            self.generic_course.objects.insert(courses, write_concern={'continue_on_error': True})
+        except me.NotUniqueError:
+            pass
 
-    def insert(self, course: Course):
-        Course.objects.insert(course)
+    def insert(self, course: AbstractCourse) -> AbstractCourse:
+        self.generic_course.objects.insert(course)
 
-    # TODO: Way too much logic in this method. Comment and fix somehow
-    def update_course(self, _code: str, _name: str):
-        c = self.get(_code)
+    # TODO: Way too much logic in this method. 
+    def update_course(self, _code: str, _name: str) -> AbstractCourse:
+        c: AbstractCourse = self.get(_code)
+        # If the course does not exist in the DB at all
         if c is None:
-            self.insert(Course(code=_code, name=_name))
+            c = self.generic_course(code=_code, name=_name)
         else:
-            if c.name == _name:
-                return
-            else:
+            # If the current course name is not the same as the updated one
+            if c.name != _name:
                 current_course_names = list(
                     map(lambda cs: cs.name,  c.course_name_scores))
 
+                # If the updated course name is in the list of suggestions already
                 if _name in current_course_names:
                     i = current_course_names.index(_name)
                     cns: CourseNameScore = c.course_name_scores[i]
                     cns.score = cns.score + 1
 
+                    # The new course name has earned enough to become the default course name for this course
                     if cns.score > SCORE_THRESHOLD:
                         c.name = cns.name
                         c.course_name_scores.pop(i)
 
                     c.course_name_scores.sort()
-
-                    c.save()
                 else:
-                    if len(c.course_name_scores) > MAX_NAME_COUNT:
+                    # Pop off the least popular course name
+                    # NOTE: Suggested course names are sorted in order of descending popularity
+                    if len(c.course_name_scores) > MAX_NAME_COUNT - 1:
                         c.course_name_scores.pop()
-
+                    
                     c.course_name_scores.append(CourseNameScore(name=_name))
-
-                    c.save()
+        c.save()
+        return c
 
     def get(self, course_code: str) -> typing.Optional[Course]:
         try:
-            return Course.objects.get(pk=course_code)
+            return self.generic_course.objects.get(pk=course_code)
         except me.DoesNotExist:
             return None
 
-    def get_many(self, course_codes: typing.List[str]) -> typing.List[Course]:
-        return list(Course.objects(pk__in=course_codes))
+    def get_many(self, course_codes: typing.List[str]) -> typing.List[AbstractCourse]:
+        return list(self.generic_course.objects(pk__in=course_codes))
 
-    def drop_courses(self):
-        Course.drop_collection()
+    def drop_collection(self, collection: me.Document):
+        collection.drop_collection()
 
 
-def insert_courses_from_dict(dao_wrapper: DAOWrapper):
-    course_dictionary = {}
-    with open(PATH_JSON_COURSES, 'r') as f:
-        course_dictionary = json.load(f)
-
+def insert_courses_from_dict(dao_wrapper: DAOWrapper, course_dict: dict):
     def tuple_to_doc(course_tuple):
-        return Course(code=course_tuple[0], name=course_tuple[1])
-
-    courses = list(map(tuple_to_doc, course_dictionary.items()))
+        return dao_wrapper.generic_course(code=course_tuple[0], name=course_tuple[1])
+    
+    courses = list(map(tuple_to_doc, course_dict.items()))
     dao_wrapper.insert_many(courses)
+
+def insert_courses_from_json(dao_wrapper: DAOWrapper, file_path: str):
+    course_dictionary = {}
+    with open(file_path, 'r') as f:
+        course_dictionary = json.load(f)
+    
+    insert_courses_from_dict(dao_wrapper, course_dictionary)
 
 
 if __name__ == '__main__':
@@ -129,6 +148,7 @@ if __name__ == '__main__':
         os.environ['DB_USER'],
         os.environ['DB_PASSWORD'],
         os.environ['DB_HOST'],
-        os.environ['DB_PORT']
+        os.environ['DB_PORT'],
+        Course
     )
-    insert_courses_from_dict(dao_wrapper)
+    insert_courses_from_json(dao_wrapper, PATH_JSON_COURSES)
