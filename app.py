3973950246c6871f12ec3a0ed19b9221b50ed68c
Arash Outadi <arash.out@gmail.com>
2019-02-21 23:02:16 -0800

    Simplify my life

--- app.py
+++ app.py
@@ -27,25 +27,8 @@ else:
 
 pattern_course = re.compile(r"c\d+")
 
-API_STATUS = "ONLINE"
 API_URL = "https://ubc-api.herokuapp.com"
-
-VERSION = "1.3"
-VERSION_KEY = "version_key"
-
 DIGEST_KEY = "digest_key"
-
-
-@app.route("/course/<string:course_code>", methods=["GET"])
-def get_course(course_code):
-    course: models.Course = dao_wrapper.get_course(course_code)
-
-    dao_wrapper.insert_log(request.path, {course.code: course.name})
-
-    if course is not None:
-        return jsonify({"code": course.code, "name": course.name})
-    else:
-        return jsonify({"code": course_code, "name": ""})
 
 
 @app.route("/courses", methods=["GET"])
@@ -67,10 +50,6 @@ def get_courses():
 
     response_dict = {}
     log_dict = {}
-
-    if VERSION_KEY in all_args:
-        response_dict[VERSION_KEY] = VERSION
-        log_dict[VERSION_KEY] = VERSION_KEY
 
     # TODO: Use this to prevent many requests
     hash_digest = "NA"
@@ -97,9 +76,7 @@ def get_courses():
 @app.route("/")
 def index():
     dao_wrapper.insert_log(request.path)
-    return render_template(
-        "index.html", version=VERSION, api_status=API_STATUS, api_url=API_URL
-    )
+    return render_template("index.html", api_url=API_URL)
 
 
 @app.after_request
--- scripts/build_and_replace.py
+++ scripts/build_and_replace.py
@@ -10,10 +10,9 @@ html_src_path = os.path.join('client', 'index.html')
 with open(html_src_path, 'r') as f:
     html_source: str = f.read()
 
-now = datetime.datetime.now()
+formatted_now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 html_source = html_source.replace(r'{{JAVASCRIPT_SOURCE}}', js_source)
-html_source = html_source.replace(r'{{DATETIME}}', now.isoformat())
-html_source = html_source.replace(r'{{DATE}}', now.strftime("%Y-%m%d"))
+html_source = html_source.replace(r'{{DATETIME}}', formatted_now)
 html_out_path = os.path.join('templates', 'index.html')
 with open(html_out_path, 'w') as f:
     f.write(html_source)
