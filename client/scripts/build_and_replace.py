6800ee823252653841d3f56504f5e9b6f21d22e5
arashout <arash.out@gmail.com>
2020-11-05 19:39:31 -0800

    Front-end code a little more clear

--- client/scripts/build_and_replace.py
+++ client/scripts/build_and_replace.py
@@ -14,7 +14,7 @@ formatted_now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 html_source = html_source. \
     replace(r'{{JAVASCRIPT_SOURCE}}', js_source). \
     replace(r'{{DATETIME}}', formatted_now). \
-    replace(r'{{API_URL}}', "localhost:5001")
+    replace(r'{{API_URL}}', "https://ogzp9le070.execute-api.us-west-2.amazonaws.com/prod")
 
 html_out_path = os.path.join('build', 'index.html')
 with open(html_out_path, 'w') as f:
